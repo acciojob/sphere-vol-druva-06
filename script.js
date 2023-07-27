6d3f216e0aa72be435cb48aa1e1ae9e34b6f53ea
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-27 20:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function volume_sphere() {
-    //Write your code here
+	const myForm = document.getElementById('MyForm')
+
+	myForm.addEventListener('submit',(event)=>{
+		const radius = document.getElementById('radius').value
+		const volume = document.getElementById('volume')
+		volume.value = (4/3)*(3.14)*(Math.pow(radius,3))
+	})
   
 } 
 
